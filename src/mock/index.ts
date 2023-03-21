29324ff505cf08825b49495aad352f2194e691a1
Егор Ревенков <revenkov.egor@gmail.com>
2023-03-21 17:19:35 +0300

    first commit

---
+++ src/mock/index.ts
@@ -0,0 +1,17 @@
+import { LocalGithubUser } from "types";
+
+export const defaultUser: LocalGithubUser = {
+    "login": "ervnkv",
+    "id": 26189056,
+    "avatar": "https://avatars.githubusercontent.com/u/26189056?v=4",
+    "name": "Yeah Gor",
+    "company": "ВОСХОД",
+    "blog": "",
+    "location": "Санкт-Петербург, Россия",
+    "bio": "React developer",
+    "twitter": "Усман Алишеров",
+    "repos": 2,
+    "followers": 0,
+    "following": 2,
+    "created": "2017-03-04T18:02:58Z",
+} 
